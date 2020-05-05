64756bf073c53223eb4134d2a46c8869baccb7cf
goomyweb <goo.myweb@gmail.com>
2020-05-05 05:57:27 +0300

    add last file in main page

--- view.py
+++ view.py
@@ -1,11 +1,26 @@
 from app import app
 from flask import render_template
-
+from models import Post, Tag
 
 @app.route('/')
 def index():
 	name = 'Gellio'
+
+	last_time = 0
+	all_posts = Post.query.all()
+	for post in all_posts:
+		if last_time != 0:
+			if post.created > last_time:
+				last_time = post.created
+				last_post = post
+		else:
+			last_time = post.created
+			last_post = post
+
+
 	context = {
-	'n': name
+	'n': name,
+	'last_post': last_post,
+	'last_time': last_time,
 	}
 	return render_template('index.html', context=context)
